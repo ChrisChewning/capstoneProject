10f664891091319fe7d8b6b36e1f9eb83fb28f78
Chris <chewning.cl@gmail.com>
2018-08-26 23:28:00 -0500

    all onClick fns working

--- src/Notepad/index.js
+++ src/Notepad/index.js
@@ -2,17 +2,50 @@ import React, {Component} from 'react';
 import {Button, Form, Icon, Row, Input} from 'react-materialize'
 
 class ToDoList extends Component {
+  constructor(){
+    super();
+    this.addNote = this.addNote.bind(this); //react docs best-preactice.
+    this.deleteNote = this.deleteNote.bind(this);
+    this.editNote = this.editNote.bind(this);
+    this.closeNote = this.closeNote.bind(this);
+  }
+
+
+  addNote(){
+    console.log('add:', this.addNote);
+   }
+
+   deleteNote(){
+     console.log('delete:', this.deleteNote);
+   }
+
+   editNote(){
+     console.log('edit:', this.editNote);
+   }
+
+   closeNote(){
+     console.log('close:', this.closeNote);
+   }
+
 
   render() {
     return (
       <div className='notesContainer'>
+        <div onClick={this.addNote}>
           <Icon className='addNewNote'>add_to_photos</Icon>
+        </div>
 
         <form className='note'>
           <div className='notesIcons'>
-          <Icon extra-small>delete</Icon>
+            <div onClick={this.deleteNote}>
+          <Icon extra-small onClick={this.deleteNote}>delete</Icon>
+          </div>
+          <div onClick={this.editNote}>
           <Icon extra-small>edit</Icon>
+          </div>
+          <div onClick={this.closeNote}>
           <Icon extra-small>close</Icon>
+          </div>
         </div>
           <Row><Input s={8} type='' className='noteTitle' label='Title' /></Row>
           <Row><Input s={12} type='textarea' className='note' label='Text' /></Row>
