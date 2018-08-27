ea0f6eb3ecefb2f3411991c577a15fdebb8fe864
Chris <chewning.cl@gmail.com>
2018-08-26 22:47:21 -0500

    stlying for notes done, materialize working

--- src/Notepad/index.js
+++ src/Notepad/index.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Button, Form, Icon, Row, Input} from 'react-materialize'
 
 class ToDoList extends Component {
 
+  render() {
+    return (
+      <div className='notesContainer'>
+          <Icon className='addNewNote'>add_to_photos</Icon>
 
-    render() {
-      return <h1>Notepad</h1>;
-    }
-  }
+        <form className='note'>
+          <div className='notesIcons'>
+          <Icon extra-small>delete</Icon>
+          <Icon extra-small>edit</Icon>
+          <Icon extra-small>close</Icon>
+        </div>
+          <Row><Input s={8} type='' className='noteTitle' label='Title' /></Row>
+          <Row><Input s={12} type='textarea' className='note' label='Text' /></Row>
+          <Button waves='light' type='submit'>Submit</Button>
+        </form>
+
+      </div>
+    )}
+}
 
 export default ToDoList;
